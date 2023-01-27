f0536b655adf7bb42a86c8d82e67566c78f92263
ShishkovEM <e.m.shishkov@yandex.ru>
2023-01-28 02:48:32 +0400

    Revert "Middleware refactor attempt"
    This reverts commit fa608228165790fb984979477f2679b4c76c4ea1.

--- internal/app/middleware/middleware.go
+++ internal/app/middleware/middleware.go
@@ -11,8 +11,6 @@ import (
 var allowedContentTypes = [...]string{
 	"application/javascript",
 	"application/json",
-	"application/x-gzip",
-	"application/gzip",
 	"text/css",
 	"text/html",
 	"text/plain",
@@ -112,77 +110,5 @@ func ZipResponse(next http.Handler) http.Handler {
 		w.Header().Set("Content-Encoding", "gzip")
 
 		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gzw}, r)
-	})
-}
-
-func GzipMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// проверяем, что клиент поддерживает gzip-сжатие
-		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			// если gzip не поддерживается, передаём управление
-			// дальше без изменений
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		// проверяем, имеет ли смысл сжимать контент
-		// если контент не входит в перечень allowedContentTypes,
-		// передаём управление дальше без изменений
-		contentType := r.Header.Get("Content-Type")
-		contentTypeOK := false
-		for _, t := range allowedContentTypes {
-			if strings.Contains(contentType, t) {
-				contentTypeOK = true
-				break
-			}
-		}
-		if !contentTypeOK {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		// создаём gzip.Writer поверх текущего w
-		gzw, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
-		if err != nil {
-			_, err := io.WriteString(w, err.Error())
-			if err != nil {
-				log.Fatalf("Error when creating gzipWriter: %s", err)
-				return
-			}
-			return
-		}
-		defer func(gzw *gzip.Writer) {
-			err := gzw.Close()
-			if err != nil {
-				log.Fatalf("Error when closing gzipWriter: %s", err)
-			}
-		}(gzw)
-
-		w.Header().Set("Content-Encoding", "gzip")
-
-		var reader io.Reader
-
-		if strings.Contains(r.Header.Get(`Content-Encoding`), "gzip") {
-			gzr, err := gzip.NewReader(r.Body)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			reader = gzr
-			defer func(gzr *gzip.Reader) {
-				err := gzr.Close()
-				if err != nil {
-					log.Fatalf("Error when closing gzipReader: %s", err)
-				}
-			}(gzr)
-			req, err := http.NewRequest(r.Method, r.RequestURI, reader)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gzw}, req)
-		} else {
-			next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gzw}, r)
-		}
 	})
 }
