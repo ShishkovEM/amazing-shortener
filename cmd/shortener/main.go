97ead7338aa0c3f527c0a335a69116da4d5b91c1
Evgenii Shishkov <e.m.shishkov@yandex.ru>
2023-01-27 15:25:37 +0400

    Middleware back

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -41,12 +41,11 @@ func main() {
 
 	// Запускаем маршрутизацию
 	router := chi.NewRouter()
-	router.Use(mw.UnzipRequest, mw.ZipResponse)
 	router.Mount("/", linkService.Routes())
 	router.Mount("/api", linkService.RestRoutes())
 
 	// Запускаем http-сервер
-	err := http.ListenAndServe(lsc.Address, router)
+	err := http.ListenAndServe(lsc.Address, mw.Conveyor(router, mw.UnzipRequest, mw.ZipResponse))
 	if err != nil {
 		log.Printf("Error starting linkService: %s\n", err)
 		return
