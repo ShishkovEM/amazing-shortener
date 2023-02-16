8274d6ae1a5238d8b57286679965295eeadaf1d4
ShishkovEM <e.m.shishkov@yandex.ru>
2023-02-16 23:22:55 +0400

    Increment 14 fix

--- internal/app/service/userservice.go
+++ internal/app/service/userservice.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"encoding/json"
+	"io"
+	"log"
 	"net/http"
 
 	"github.com/ShishkovEM/amazing-shortener/internal/app/middleware"
@@ -12,7 +14,8 @@ import (
 
 func (ls *LinkService) UserLinkRoutes() chi.Router {
 	r := chi.NewRouter()
-	r.Get("/urls", ls.getLinksByUserIDHandler) // Получение ссылок, созданных полизователем
+	r.Get("/urls", ls.getLinksByUserIDHandler)  // Получение ссылок, созданных полизователем
+	r.Delete("/urls", ls.deleteUserURLsHandler) // Удаление ссылок
 	return r
 }
 
@@ -47,4 +50,49 @@ func (ls *LinkService) getLinksByUserIDHandler(w http.ResponseWriter, r *http.Re
 	if err != nil {
 		return
 	}
+}
+
+func (ls *LinkService) deleteUserURLsHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling link delete via #deleteUserURLsHandler at %s\n", req.URL.Path)
+	w.Header().Set("content-type", "application/json")
+
+	rawUserID := req.Context().Value(middleware.ContextKeyUserID)
+	var userID uint32
+
+	switch uidType := rawUserID.(type) {
+	case uint32:
+		userID = uidType
+	}
+
+	var request []string
+
+	b, _ := io.ReadAll(req.Body)
+	err := json.Unmarshal(b, &request)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, `{"error":"Invalid body"}`, http.StatusBadRequest)
+		return
+	}
+
+	if len(request) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, `{"error":"Body must be not empty array"}`, http.StatusBadRequest)
+		return
+	}
+
+	go func() {
+		err = ls.store.DeleteUserRecordsByShortURLs(userID, request)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, `{"error":"Failed to delete user records by short url"}`, http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
 }
